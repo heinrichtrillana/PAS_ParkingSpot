d9b32313f9fc7e123645f8547431fb2473152bbe
Heinrich <heinrich.trillana.revita@alumnos.upm.es>
2020-06-06 01:29:30 +0200

    Map added
    Fragment con el mapa añadido

--- app/src/main/java/com/example/myparkingspot/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/myparkingspot/ui/map/MapFragment.kt
@@ -1,19 +1,35 @@
 package com.example.myparkingspot.ui.map
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.myparkingspot.R
+import com.google.android.gms.location.FusedLocationProviderClient
 
-class MapFragment : Fragment() {
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import kotlinx.android.synthetic.main.fragment_map.*
+
+
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mapViewModel: MapViewModel
+    private lateinit var googleMap: GoogleMap
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+        mapView.getMapAsync(this)
+        requestPermissions() //Pedir permisos al iniciar la actividad;
+
+    }
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -21,11 +37,42 @@ class MapFragment : Fragment() {
     ): View? {
         mapViewModel =
                 ViewModelProviders.of(this).get(MapViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        mapViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val root = inflater.inflate(R.layout.fragment_map, container, false)
         return root
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        map?.let{
+             googleMap = it
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray){
+        when (requestCode) {
+            0 -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Si nos da permiso, habilitamos la capa y centramos en su ubicacion
+                    googleMap.isMyLocationEnabled = true
+                }
+                return
+            }
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+    private fun requestPermissions() {
+        val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        requestPermissions(permissions,0)
+    }
+
+    private fun checkPermissions(): Boolean {
+        context?.let{
+            if (ActivityCompat.checkSelfPermission(it, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(it,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            )   return true
+        }
+        return false
     }
 }
