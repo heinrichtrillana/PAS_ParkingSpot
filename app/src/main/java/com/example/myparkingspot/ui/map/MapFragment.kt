3aa2c8be4d4d5eb3ef89d653c470215a4c5882f0
Heinrich <heinrich.trillana.revita@alumnos.upm.es>
2020-06-06 04:06:32 +0200

    Location simple functionality
    - Save current location as last location on shared pref.
    - Show last location saved adding a marker.

--- app/src/main/java/com/example/myparkingspot/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/myparkingspot/ui/map/MapFragment.kt
@@ -1,18 +1,30 @@
 package com.example.myparkingspot.ui.map
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.myparkingspot.R
+import com.example.myparkingspot.databinding.FragmentMapBinding
 import com.google.android.gms.location.FusedLocationProviderClient
-
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_map.*
 
 
@@ -20,7 +32,11 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mapViewModel: MapViewModel
     private lateinit var googleMap: GoogleMap
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var binding : FragmentMapBinding
+
+    private lateinit var lastLocation : Marker
+    private lateinit var sharedPref : SharedPreferences
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -29,7 +45,12 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         mapView.getMapAsync(this)
         requestPermissions() //Pedir permisos al iniciar la actividad;
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        sharedPref = activity?.getSharedPreferences(
+            getString(R.string.location_preference_file), Context.MODE_PRIVATE) ?: return
     }
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -37,8 +58,14 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     ): View? {
         mapViewModel =
                 ViewModelProviders.of(this).get(MapViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_map, container, false)
-        return root
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_map, container, false)
+
+        binding.addLocation.setOnClickListener{onAddLocationClicked()}
+        binding.lastLocation.setOnClickListener{onSeeLocationClicked()}
+
+
+        return binding.root
     }
 
     override fun onMapReady(map: GoogleMap?) {
@@ -53,6 +80,17 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                     // Si nos da permiso, habilitamos la capa y centramos en su ubicacion
                     googleMap.isMyLocationEnabled = true
+
+                    lastLocation = googleMap.addMarker(MarkerOptions().position(LatLng(0.0,0.0)))
+                    lastLocation.isVisible = false
+
+                    //Mover la camara a mi ubicacion
+                    fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
+                        location?.let {
+                            var posi = LatLng(it.latitude, it.longitude)
+                            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(posi,18F));
+                        }
+                    }
                 }
                 return
             }
@@ -61,6 +99,66 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             }
         }
     }
+
+    private fun onSeeLocationClicked(){
+        //lastLocation = googleMap.addMarker(MarkerOptions().position(LatLng(lat,long)))
+        //lastLocation.isVisible = false
+
+        if(lastLocation.isVisible){ //Si estaba seleccionada la deselecciono
+            binding.lastLocation.setImageResource(R.drawable.ic_location_black_24dp)
+            //Esconder pin
+            lastLocation.isVisible = false;
+        } else {
+            //Sacar los valores del shared preferences
+            if( sharedPref.contains(getString(R.string.latitude_key))
+                && sharedPref.contains(getString(R.string.longitude_key))){ //Si existe la posicion anterior, la muestro
+
+                binding.lastLocation.setImageResource(R.drawable.ic_location_off_black_24dp)
+                var lat = sharedPref.getString(getString(R.string.latitude_key), "");
+                var long = sharedPref.getString(getString(R.string.longitude_key), "");
+
+                var latLng = LatLng(lat!!.toDouble(),long!!.toDouble())
+
+                //añadir pin
+                lastLocation = googleMap.addMarker(MarkerOptions().position(latLng))
+                lastLocation.isVisible = true;
+                //Mover hacia el pin
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,18F));
+            } else { //Si no , alerto
+                Snackbar.make(mapView, getString(R.string.location_view_last_error), Snackbar.LENGTH_SHORT).show();
+            }
+
+        }
+
+    }
+
+    private fun onAddLocationClicked(){
+        if(!checkPermissions()){ //Si no tiene permisos de Localizacion los pide.
+            requestPermissions()
+            return;
+        }
+        if(!isLocationEnabled()){ //Si no tiene la ubicacion activada, pide que se active
+            Snackbar.make(mapView, getString(R.string.add_location_location_error), Snackbar.LENGTH_SHORT).show();
+            return;
+        }
+        //Cuando todo esta bien, se obitene la localizacion y se guarda.
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                // Got last known location. In some rare situations this can be null.
+
+                with(sharedPref.edit()) {
+                    putString(getString(R.string.latitude_key),location!!.latitude.toString())
+                    putString(getString(R.string.longitude_key),location!!.longitude.toString())
+                    commit()
+                }
+
+                lastLocation.isVisible = false //Si habia un marcador, al añadir uno nuevo escondemos el anterior.
+                binding.lastLocation.setImageResource(R.drawable.ic_location_black_24dp)
+
+                Snackbar.make(mapView, getString(R.string.location_added_success), Snackbar.LENGTH_SHORT).show();
+            }
+    }
+
 
     private fun requestPermissions() {
         val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
@@ -74,5 +172,12 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             )   return true
         }
         return false
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
     }
 }
